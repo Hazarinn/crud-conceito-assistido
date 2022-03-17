2d87f70823447014d18a6214da715c814f2b9548
Rubens Diego <devbsb.rubens@gmail.com>
2022-03-17 14:22:18 -0300

    iniciando projeto

---
+++ src/main/java/com/defensoria/crudconceitoassistido/CrudConceitoAssistidoApplication.java
@@ -0,0 +1,13 @@
+package com.defensoria.crudconceitoassistido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudConceitoAssistidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudConceitoAssistidoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/defensoria/crudconceitoassistido/CrudConceitoAssistidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.defensoria.crudconceitoassistido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudConceitoAssistidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
